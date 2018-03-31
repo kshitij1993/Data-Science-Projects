35c464e6c19907764a35d58b7224b1bb724a51d6
kshitij1993 <31812862+kshitij1993@users.noreply.github.com>
2018-03-30 21:33:35 -0400

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,70 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+print("Data Science Assignment 1")
+
+
+# In[2]:
+
+
+print("Kshitij Sale")
+
+
+# In[14]:
+
+
+print("How to concatenate multiple strings?")#using +
+
+
+# In[8]:
+
+
+variable1 = 'I'
+variable2 = ' am a'
+variable3 = ' boy'
+
+print("About me :",variable1+variable2+variable3)
+
+
+# In[15]:
+
+
+print("How to capitalise ?") # using capitalize()
+
+
+# In[10]:
+
+
+string1='this is a beautiful place'
+
+print("Capitalized string : ",string1.capitalize())
+
+
+# In[11]:
+
+
+print("How to update strings ?")
+
+
+# In[13]:
+
+
+string1='this is a beautiful place'
+print("Updated String :",string1[:20]+'art')
+
+
+# In[16]:
+
+
+print("How to get a pattern of strings ?")#using *
+
+
+# In[17]:
+
+
+variable1 = ' boy'
+print("4 times boy : ",variable1*4)
+
